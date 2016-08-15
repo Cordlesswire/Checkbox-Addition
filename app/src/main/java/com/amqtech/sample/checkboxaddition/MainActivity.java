5daab41b86df7e8325189586b4b0b03e962e4177
Andrew Quebe <andrewquebe@amqtech.com>
2016-08-15 16:31:07 -0700

    Initial commit

---
+++ app/src/main/java/com/amqtech/sample/checkboxaddition/MainActivity.java
@@ -0,0 +1,13 @@
+package com.amqtech.sample.checkboxaddition;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
